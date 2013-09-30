3a6f09dd6463c93e9a4a9374706d1d4329c8fc7b
Matt Carter <m@ttcarter.com>
2013-09-30 16:12:22 +1000

    Updated RegExp for Virtual Shackles

--- comics/virtual-shackles.php
+++ comics/virtual-shackles.php
@@ -4,6 +4,6 @@ $cg->verbosity(2);
 $cg->filename('[filename].[ext]');
 $cg->nodate();
 $cg->start('http://www.virtualshackles.com/');
-$cg->match('image', '<div id="comicbox">\s*<img src="(/img/.*?)" .*? alt=".*?"\s*/>\s*</div>');
+$cg->match('image', '<meta name="og:image" content="(.*?)" />');
 $cg->match('previous', '<a href ="(/[0-9]+)" title="Previous">');
 $cg->grab();
