d0206c55f90fbb0964ffd46167432722e3a5ddc9
Matt Carter <m@ttcarter.com>
2013-09-30 16:04:03 +1000

    Updated the Surviving the World plugin

--- comics/surviving-the-world.php
+++ comics/surviving-the-world.php
@@ -4,6 +4,6 @@ $cg->verbosity(2);
 $cg->start('http://survivingtheworld.net');
 $cg->filename('[filename].[ext]');
 $cg->nodate();
-$cg->match('image', '<tr>\s*<td colspan="4" rowspan="[0-9]+" align="right" valign="top"><img src="(.*?)"');
-$cg->match('previous', '<td width="120" valign="top"><a href="([a-zA-Z0-9\.]+?)" onmouseout="MM_swapImgRestore\(\)" onmouseover="MM_swapImage\(\'Previous\'');
+$cg->match('image', '<div class="img">.*?<img src="(.*?)".*?/>.*?</div>');
+$cg->match('previous', '<li class="previous">.*?<a href="(.*?)">.*?</a>.*?</li>');
 $cg->grab();
