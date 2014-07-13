c28a9568230adc08a39b65e1213b06be804fcedb
Matt Carter <m@ttcarter.com>
2014-07-13 11:03:41 +1000

    Updated PA script

--- comics/pa.php
+++ comics/pa.php
@@ -2,7 +2,7 @@
 $cg = new ComicGrabber('Penny Arcade');
 $cg->verbosity(2);
 $cg->start('http://www.penny-arcade.com/comic/');
-$cg->match('date', '<li><a class="btnNews btn" href="http://www.penny-arcade.com/([0-9]{4})/([0-9]{1,2})/([0-9]{1,2})" title="News">News</a></li>', '$m_1-$m_2-$m_3');
-$cg->match('image', '<div class="post comic">.*?<img src="(.+?)" alt=".*?"\s*/>.*?</div>');
-$cg->match('previous', '<li><a class="btnPrev btn" href="(.+?)" title="Previous">Previous</a></li>');
+$cg->match('date', '<h4><a href="http://www.penny-arcade.com/news/post/([0-9]{4})/([0-9]{1,2})/([0-9]{1,2})/.*" title=".*?">Read News Post</a></h4>', '$m_1-$m_2-$m_3');
+$cg->match('image', '<div id="comicFrame"><a href=".*?" title="Next"><img src="(.+?)".*?/>');
+$cg->match('previous', '<li><a class="btn btnPrev" href="(.+?)" title="Previous">Previous</a></li>');
 $cg->grab();
