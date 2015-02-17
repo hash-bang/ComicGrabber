28e7a6565b9bee79137c857049fd0b37a42880b0
Matt Carter <m@ttcarter.com>
2015-02-18 09:22:50 +1000

    Updated Dilbert grabber

--- comics/dilbert.php
+++ comics/dilbert.php
@@ -1,8 +1,10 @@
 <?
 $cg = new ComicGrabber('Dilbert');
 $cg->verbosity(2);
-$cg->start('http://dilbert.com');
+$cg->filename('[year][month][day].gif');
+$cg->start('http://dilbert.com/strip/' . date('Y-m-d')); // Have to request todays date explicitly
 $cg->match('date', '<div class="STR_Content">.+?<div class="STR_Image">.+?<a href="/strips/comic/([0-9]{4}-[0-9]{2}-[0-9]{2})/">');
-$cg->match('image', '<div class="STR_Content">.+?<div class="STR_Image">.+?<a href="/strips/comic/[0-9]{4}-[0-9]{2}-[0-9]{2}/"><img .*? src="(.+?)"');
-$cg->match('previous', '<a href="(/[0-9]{4}-[0-9]{2}-[0-9]{2}/)" class="STR_Prev.*?"><span>Previous</span></a>');
+$cg->match('date', '<div class="comic-item-container js_comic_container_([0-9]{4}-[0-9]{2}-[0-9]{2})"');
+$cg->match('image', '<img alt=".*?" class="img-responsive img-comic".*?src="(http://assets.amuniversal.com/.*?)".*?/>');
+$cg->match('previous', '<div class="nav-comic nav-left">.*?<a href="(/strip/.*?)" alt="Older Strip">');
 $cg->grab();
